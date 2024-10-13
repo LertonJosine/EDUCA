c980291e698cdca85491393b2135efcdc0dc1ff6
LertonJosine <josinelerton@gmail.com>
2024-10-13 14:37:23 +0200

    render host corrected for allowed hosts

--- educa/settings.py
+++ educa/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'educa-hadq.onrender.com']
 
 
 
