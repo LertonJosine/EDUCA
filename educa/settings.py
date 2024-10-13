924bbeeab4c1b2ca0b1e7aa3f1357157a30d8a9d
LertonJosine <josinelerton@gmail.com>
2024-10-13 15:33:54 +0200

    attempt to solve staticfiles serving

--- educa/settings.py
+++ educa/settings.py
@@ -1,7 +1,7 @@
 
 from pathlib import Path
 from environs import Env
-
+import os
 
 env = Env()
 env.read_env()
@@ -116,11 +116,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
